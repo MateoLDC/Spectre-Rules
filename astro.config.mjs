f4416462b015a0077ee83fabcfa819d9845f8e4c
MateoLDC <leducmateo@gmail.com>
2026-07-30 12:32:21 +0200

    config astro for gh page in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
+    site: "https://mateoldc.github.io",
+    base: "/Spectre-Rules/",
     integrations: [
         starlight({
             title: "Spectre",
